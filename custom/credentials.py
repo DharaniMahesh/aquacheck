28dd05b4787c4dd2e64333de46db6361da1a7b7f
DDM <114044922+DharaniMahesh@users.noreply.github.com>
2024-03-23 10:08:57 +0530

    Update credentials.py

--- custom/credentials.py
+++ custom/credentials.py
@@ -1,2 +1,3 @@
-token = 'bb9a996e9a10bd77af7596beb93469dd'#'mvgJM3BLJuAfQeRNoITyO9BNNhIpaUZa' 
-account = 'AC2b6a2ee5a05e20636e5ee03d04d2f4fe' #'SKc959bdfe493d893b674780e5e27991c7' 
+import os
+token = os.environ.get("token")#'mvgJM3BLJuAfQeRNoITyO9BNNhIpaUZa' 
+account = os.environ.get("account") #'SKc959bdfe493d893b674780e5e27991c7' 
